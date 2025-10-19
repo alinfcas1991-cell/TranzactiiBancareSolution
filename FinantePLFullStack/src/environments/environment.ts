0c86992e87323eafdadedcd8a20a5dec763101ad
alinfcas1991-cell <alinfcas1991@gmail.com>
2025-10-19 10:41:05 +0300

    Force rebuild Angular dist for Render

--- FinantePLFullStack/src/environments/environment.ts
+++ FinantePLFullStack/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tranzactiibancaresolution.onrender.com', // ⬅️ modifică aici
+  apiUrl: `https://tranzactiibancaresolution.onrender.com`, // ⬅️ modifică aici
   allowConsoleLogs: true
 };
